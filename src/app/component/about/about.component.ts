5a36455409dcc00d55a54d1ce843a0b72f1c660b
shanuka chathuranga <shanukachathuranga21@gmail.com>
2025-02-11 07:53:25 +0530

    Observabble/ debounce

--- src/app/component/about/about.component.ts
+++ src/app/component/about/about.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {debounce, debounceTime, Observable, Subscriber} from 'rxjs';
+import {log} from 'node:util';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+
+
+  searchText = new FormControl();
+
+  ngOnInit(): void {
+
+    // this.searchText.valueChanges.subscribe(value=>{
+    //   console.log(`will make the request [${value}]`);
+    // })
+
+    this.searchText.valueChanges.pipe(
+      debounceTime(500)
+    ).subscribe(value => {
+      console.log(`will make the request [${value}]`);
+    })
+
+
+  }
 
 }
